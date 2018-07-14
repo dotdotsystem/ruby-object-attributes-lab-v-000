a87ea827d1e52186235171e28d38b86b9a161907
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-14 00:03:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,16 @@
 class Dog
-
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+def name
+    @name
   end
 
-  def name
-    puts @this_dogs_name
+def name=(new_name)
+    @name = new_name
   end
+def breed
+      @breed
+    end
 
-  def breed=(dog_breed)
-    @this_dogs_breed = dog_breed
-  end
-
-  def breed
-    puts @this_dogs_breed
-  end
+    def job=(new_breed)
+      @breed = new_breed
+    end
 end
